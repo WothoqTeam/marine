f292e98a44ef729111ea9387fb4f8a3572de37a9
mohammed hefny <129668476+moohefny@users.noreply.github.com>
2023-09-06 16:16:19 +0300

    push project

---
+++ app/Http/Requests/Api/Auth/UserLoginRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Api\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserLoginRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'phone' => 'required|number|unique:users',
+            'password' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success'   => false,
+                'message'   => 'Invalid data',
+                'data'      => $validator->errors()
+            ],400)
+        );
+    }
+}
