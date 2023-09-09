4451f1bf0e0540407398977ae0ea88310e270103
mohammed hefny <129668476+moohefny@users.noreply.github.com>
2023-09-10 02:35:45 +0300

    update lang

--- resources/lang/ar/lang.php
+++ resources/lang/ar/lang.php
@@ -6,6 +6,10 @@ return [
     "dark" => "غامق",
     "system" => "حسب النظام",
     "lang" => "اللغة",
-    'Success'=>'تم بنجاح',  
-  'User successfully signed out' => 'User successfully signed out',
+    'Success'=>'تم بنجاح',
+    'User successfully signed out' => 'تم تسجيل خروج المستخدم بنجاح',
+    'invalid data' => 'بيانات غير صالحة',
+    'Profile updated successfully' => 'تم تحديث الملف الشخصي بنجاح',
+    'Invalid Unauthorized' => 'غير مصرح به غير صالح',
+    'invalid code' => 'رمز غير صالح',
 ];
