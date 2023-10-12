410026cdd63a1e086a02873c5373057c217db1ed
mohammed hefny <129668476+moohefny@users.noreply.github.com>
2023-10-12 17:01:18 +0300

    update Notifications

--- app/Http/Controllers/Api/NotificationsApiController.php
+++ app/Http/Controllers/Api/NotificationsApiController.php
@@ -10,7 +10,7 @@ class NotificationsApiController extends BaseApiController
 {
     public function index(){
         $user=auth('api')->user();
-        $notifications=Notifications::select('id','title_'.$this->language.' as title','body_'.$this->language.' as body')->where('type',User::class)->where('user_id',$user->id)->orWhere('user_id',null)->where('created_at','>=',$user->created_at)->get()->toArray();
+        $notifications=Notifications::select('id','title_'.$this->language.' as title','body_'.$this->language.' as body','created_at as date')->where('type',User::class)->where('user_id',$user->id)->orWhere('user_id',null)->where('created_at','>=',$user->created_at)->get()->toArray();
         return $this->generateResponse(true,'Success',$notifications);
     }
 }
