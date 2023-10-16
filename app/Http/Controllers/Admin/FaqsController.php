ebe5b1ef2049e4b179ae867d2542041c58c1a38b
mohammed hefny <129668476+moohefny@users.noreply.github.com>
2023-10-16 14:37:31 +0300

    Create Faqs Admin

---
+++ app/Http/Controllers/Admin/FaqsController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Faqs;
+use Illuminate\Http\Request;
+use DataTables;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Validator;
+use Illuminate\Support\Facades\App;
+class FaqsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if (request()->ajax()) {
+            $data = Faqs::query();
+
+            return Datatables::of($data)
+                ->addColumn('checkbox', function($row){
+                    $checkbox = '<div class="form-check form-check-sm p-3 form-check-custom form-check-solid">
+                                    <input class="form-check-input" type="checkbox" value="'.$row->id.'" />
+                                </div>';
+                    return $checkbox;
+                })
+                ->addColumn('name', function($row){
+                    if (App::getLocale()=='en'){
+                        $name=$row->title_en;
+                    }else{
+                        $name=$row->title_ar;
+                    }
+                    $name = '<div class="d-flex flex-column"><a href="javascript:;" class="text-gray-800 text-hover-primary mb-1">'.$name.'</a>';
+                    $name .= '<span>'.$row->email.'</span></div>';
+                    return $name;
+                })
+                ->addColumn('status', function($row){
+                    if($row->status == 1) {
+                        $is_active = '<div class="badge badge-light-success fw-bold">'.trans('labels.inputs.active').'</div>';
+                    } else {
+                        $is_active = '<div class="badge badge-light-danger fw-bold">'.trans('labels.inputs.in_active').'</div>';
+                    }
+
+                    return $is_active;
+                })
+                ->addColumn('actions', function($row){
+                    $actions = '<div class="ms-2">
+                                <a href="'.route('admin.faqs.show', $row->id).'" class="btn btn-sm btn-icon btn-warning btn-active-dark me-2" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                    <i class="bi bi-eye-fill fs-1x"></i>
+                                </a>
+                                <a href="'.route('admin.faqs.edit', $row->id).'" class="btn btn-sm btn-icon btn-info btn-active-dark me-2" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                    <i class="bi bi-pencil-square fs-1x"></i>
+                                </a>
+                            </div>';
+                    return $actions;
+                })
+                ->filter(function ($instance) use ($request) {
+                    if ($request->get('status') == '0' || $request->get('status') == '1') {
+                        $instance->where('status', $request->get('status'));
+                    }
+                    if (!empty($request->get('search'))) {
+                        $instance->where(function($w) use($request){
+                            $search = $request->get('search');
+                            $w->orWhere('name_ar', 'LIKE', "%$search%")
+                                ->orWhere('name_en', 'LIKE', "%$search%");
+                        });
+                    }
+                })
+                ->rawColumns(['name','status','checkbox','actions'])
+                ->make(true);
+        }
+
+        return view('admin.faqs.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.faqs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rule = [
+            'title_en' => 'required|string',
+            'title_ar' => 'required|string',
+            'body_en' => 'required|string',
+            'body_ar' => 'required|string',
+        ];
+
+        $validate = Validator::make($request->all(), $rule);
+        if ($validate->fails()) {
+            return redirect()->back()->with('message', $validate->messages()->first())->with('status', 'error')->withInput($request->all());
+        }
+        $row = Faqs::create([
+            'title_en' => $request->title_en,
+            'title_ar' => $request->title_ar,
+            'body_en' => $request->body_en,
+            'body_ar' => $request->body_ar,
+            'status' => $request->status ?? 0,
+        ]);
+        return redirect('admin/faqs')->with('message', trans('labels.labels.added_successfully'))->with('status', 'success');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $data=Faqs::find($id);
+        return view('admin.faqs.show', compact('data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $data =Faqs::find($id);
+        return view('admin.faqs.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $rule = [
+            'title_en' => 'required|string',
+            'title_ar' => 'required|string',
+            'body_en' => 'required|string',
+            'body_ar' => 'required|string',
+        ];
+
+        $validate = Validator::make($request->all(), $rule);
+        if ($validate->fails()) {
+            return redirect()->back()->with('message', $validate->messages()->first())->with('status', 'error')->withInput($request->all());
+        }
+
+        $data = Faqs::find($request->id);
+        $data->update([
+            'title_en' => $request->title_en,
+            'title_ar' => $request->title_ar,
+            'body_en' => $request->body_en,
+            'body_ar' => $request->body_ar,
+            'status' => $request->status ?? 0,
+        ]);
+        return redirect('admin/faqs')->with('message', trans('labels.labels.modified_successfully'))->with('status', 'success');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        try{
+            Faqs::whereIn('id',$request->id)->delete();
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'error']);
+        }
+        return response()->json(['message' => 'success']);
+    }
+}
---
+++ resources/views/admin/faqs/create.blade.php
@@ -0,0 +1,97 @@
+@extends('admin.layout.master')
+
+@section('css')
+@endsection
+
+@section('style')
+
+@endsection
+
+@section('breadcrumb')
+<div class="page-title d-flex flex-column justify-content-center gap-1 me-3 pt-6">
+    <!--begin::Title-->
+    <h1 class="page-heading d-flex flex-column justify-content-center text-dark fw-bold fs-3 m-0">{{trans('labels.labels.add_new')}}</h1>
+    <!--end::Title-->
+    <!--begin::Breadcrumb-->
+    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0">
+        <!--begin::Item-->
+        <li class="breadcrumb-item text-muted">
+            <a href="{{route('admin.faqs.index')}}" class="text-muted text-hover-primary">{{trans('labels.settings.faq_tap')}}</a>
+        </li>
+        <!--end::Item-->
+        <!--begin::Item-->
+        <li class="breadcrumb-item">
+            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+        </li>
+        <!--end::Item-->
+        <!--begin::Item-->
+        <li class="breadcrumb-item text-muted">{{trans('labels.labels.add_new')}}</li>
+        <!--end::Item-->
+    </ul>
+    <!--end::Breadcrumb-->
+</div>
+@endsection
+
+@section('content')
+
+    <div id="kt_app_content_container" class="app-container container-fluid">
+        <div class="card mb-5 mb-xl-10">
+            <!--begin::Content-->
+            <div id="kt_account_settings_profile_details" class="collapse show">
+                <!--begin::Form-->
+                <form action="{{route('admin.faqs.store')}}" method="POST" enctype="multipart/form-data" id="kt_account_profile_details_form" class="form">
+                    @csrf
+                    <!--begin::Card body-->
+                    <div class="card-body border-top p-9">
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.title')}} EN</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="title_en" placeholder="{{trans('labels.inputs.title')}} EN" value="{{old('title_en')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.title')}} AR</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="title_ar" placeholder="{{trans('labels.inputs.title')}} AR" value="{{old('title_ar')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.body')}} EN</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea name="body_en" placeholder="{{trans('labels.inputs.body')}} EN" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" >{{old('body_en')}}</textarea>
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.body')}} AR</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea name="body_ar" placeholder="{{trans('labels.inputs.body')}} AR" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" >{{old('body_ar')}}</textarea>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <label class="col-lg-2 col-form-label fw-semibold fs-6"> {{trans('labels.inputs.status')}}</label>
+                            <div class="col-lg-8 d-flex align-items-center">
+                                <div class="form-check form-check-solid form-switch form-check-custom fv-row">
+                                    <input class="form-check-input w-45px h-30px" type="checkbox" name="status" value="1" id="allowmarketing" checked="checked" />
+                                    <label class="form-check-label" for="allowmarketing"></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer d-flex justify-content-end py-6 px-9">
+                        <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">{{trans('labels.inputs.save')}}</button>
+                    </div>
+                    <!--end::Actions-->
+                </form>
+                <!--end::Form-->
+            </div>
+            <!--end::Content-->
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+@endsection
