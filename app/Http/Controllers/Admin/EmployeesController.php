85921db19fcd8de706d0df2ead4a7ccc44934ea8
mohammed hefny <129668476+moohefny@users.noreply.github.com>
2023-10-17 14:51:57 +0300

    Create Providers Module Admin

--- app/Http/Controllers/Admin/EmployeesController.php
+++ app/Http/Controllers/Admin/EmployeesController.php
@@ -14,47 +14,6 @@ use Validator;
 
 class EmployeesController extends Controller
 {
-    public function transAdmin($key, $placeholder = [])
-    {
-        $locale = strtolower(request()->header('Language', 'en'));
-        $group = 'permissions';
-        if (is_null($locale))
-            $locale = config('app.locale');
-        $key = trim($key);
-        $word = $group . '.' . $key;
-        if (Lang::has($word))
-            return trans($word, $placeholder, $locale);
-
-        $messages = [
-            $word => $key,
-        ];
-
-
-        app('translator')->addLines($messages, $locale);
-        $translation_file = base_path() . '/resources/lang/' . $locale . '/' . $group . '.php';
-        $fh = fopen($translation_file, 'r+');
-        $new_key = "  \n  '" . $key . "' => '" . $key . "',\n];\n";
-        $saved_cursor = -1;
-        $end_with_comma = false;
-        $has_single_qoute = false;
-        for ($cursor = -1; $cursor < 100; $cursor--) {
-            fseek($fh, $cursor, SEEK_END);
-            if (fgetc($fh) == ',' && !$has_single_qoute) {
-                $end_with_comma = true;
-                fseek($fh, $cursor + 1, SEEK_END);
-                fwrite($fh, $new_key);
-                break;
-            } else if (fgetc($fh) == '\'' && !$end_with_comma) {
-                $has_single_qoute = true;
-                fseek($fh, $cursor + 2, SEEK_END);
-                fwrite($fh, ',' . $new_key);
-                break;
-            }
-        }
-        fclose($fh);
-        return trans($word, $placeholder, $locale);
-    }
-
     public function index(Request $request)
     {
         if ($request->ajax()) {
