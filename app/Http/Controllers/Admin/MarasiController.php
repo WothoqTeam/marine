833368a193aec9fdfca5f402f99fc14827eef5a7
mohammed hefny <129668476+moohefny@users.noreply.github.com>
2023-10-29 13:11:59 +0200

    Create Marasi Admin Module

---
+++ app/Http/Controllers/Admin/MarasiController.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cities;
+use App\Models\Countries;
+use App\Models\Employee;
+use App\Models\Marasi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use DataTables;
+use Illuminate\Support\Facades\Validator;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class MarasiController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        if ($request->ajax()) {
+            $data = Marasi::query();
+            $data = $data->orderBy('id', 'DESC');
+
+            return Datatables::of($data)
+                ->addColumn('checkbox', function ($row) {
+                    $checkbox = '<div class="form-check form-check-sm p-3 form-check-custom form-check-solid">
+                                    <input class="form-check-input" type="checkbox" value="' . $row->id . '" />
+                                </div>';
+                    return $checkbox;
+                })
+                ->addColumn('name', function ($row) {
+                    if (App::getLocale() == 'en') {
+                        $name = $row->name_en;
+                    } else {
+                        $name = $row->name_ar;
+                    }
+                    $name = '<div class="d-flex flex-column"><a href="javascript:;" class="text-gray-800 text-hover-primary mb-1">' . $name . '</a>';
+                    return $name;
+                })
+                ->addColumn('price', function ($row) {
+
+                    $price = $row->price;
+
+                    return $price;
+                })
+                ->addColumn('is_approved', function ($row) {
+                    $is_approved = '<select name="is_approved" id="changeStatus" data-control="select2" data-hide-search="true" class="form-select form-select-solid fw-bold" onchange="changeMarasiStatus(' . $row->id . ')"> <option value="1"';
+                    $is_approved .= $row->is_approved == 1 ? "selected" : "";
+                    $is_approved .= '>' . trans("labels.inputs.active") . '</option> <option value="0"';
+                    $is_approved .= $row->is_approved == 0 ? "selected" : "";
+                    $is_approved .= '>' . trans("labels.inputs.in_active") . '</option></select>';
+
+                    return $is_approved;
+                })
+                ->addColumn('actions', function ($row) {
+                    $actions = '<div class="ms-2">
+                                <a href="' . route('admin.marasi.show', $row->id) . '" class="btn btn-sm btn-icon btn-warning btn-active-dark me-2" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                    <i class="bi bi-eye-fill fs-1x"></i>
+                                </a>
+                                <a href="' . route('admin.marasi.edit', $row->id) . '" class="btn btn-sm btn-icon btn-info btn-active-dark me-2" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                    <i class="bi bi-pencil-square fs-1x"></i>
+                                </a>
+                            </div>';
+                    return $actions;
+                })
+                ->filter(function ($instance) use ($request) {
+                    if ($request->get('is_approved') == '0' || $request->get('is_approved') == '1') {
+                        $instance->where('is_approved', $request->get('is_approved'));
+                    }
+                    if (!empty($request->get('search'))) {
+                        $instance->where(function ($w) use ($request) {
+                            $search = $request->get('search');
+                            $w->orWhere('name_ar', 'LIKE', "%$search%")
+                                ->orWhere('name_en', 'LIKE', "%$search%");
+                        });
+                    }
+                })
+                ->rawColumns(['name', 'price', 'is_approved', 'checkbox', 'actions'])
+                ->make(true);
+        }
+        return view('admin.marasi.index');
+    }
+
+    public function show($id)
+    {
+        $data = Marasi::with('city', 'country', 'employee')->find($id);
+        return view('admin.marasi.show', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $data['countries'] = Countries::select('id', 'name_' . App::getLocale() . ' as name')->where('status', 1)->get();
+        $data['cities'] = Cities::select('id', 'name_' . App::getLocale() . ' as name')->where('status', 1)->get();
+        $data['owner'] = Employee::select('id', 'name_ar as name')->where('role_id','2')->where('is_active', '1')->get();
+        return view('admin.marasi.create',compact('data'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rule = [
+            'employee_id' => 'required|int|exists:employees,id',
+            'name_en' => 'required|string',
+            'name_ar' => 'required|string',
+            'description_en' => 'required|string',
+            'description_ar' => 'required|string',
+            'address_en' => 'required|string',
+            'address_ar' => 'required|string',
+            'price' => 'required|numeric',
+            'photo' => 'required|image|mimes:png,jpg,jpeg|max:2048',
+            'country_id' => 'required|int|exists:countries,id',
+            'city_id' => 'required|int|exists:cities,id',
+            'longitude' => 'required|string',
+            'latitude' => 'required|string',
+            'covers' => 'required|array|min:1',
+            'covers.*' => 'image|mimes:jpeg,png|max:1024', // Validate each image
+        ];
+
+        $validate = Validator::make($request->all(), $rule);
+        if ($validate->fails()) {
+            return redirect()->back()->with('message', $validate->messages()->first())->with('status', 'error')->withInput($request->all());
+        }
+        $row = Marasi::create([
+            'employee_id'=>$request->employee_id,
+            'name_en'=>$request->name_en,
+            'name_ar'=>$request->name_ar,
+            'description_en'=>$request->description_en,
+            'description_ar'=>$request->description_ar,
+            'address_en'=>$request->address_en,
+            'address_ar'=>$request->address_ar,
+            'longitude'=>$request->longitude,
+            'latitude'=>$request->latitude,
+            'price'=>$request->price,
+            'country_id'=>$request->country_id,
+            'city_id'=>$request->city_id,
+            'is_discount'=>is_null($request->is_discount) ? 0 : 1,
+            'discount_value'=>is_null($request->is_discount) ? 0 : $request->discount,
+            'status' => $request->status ?? 0,
+        ]);
+
+        if($request->hasFile('photo') && $request->file('photo')->isValid()){
+            $row->addMediaFromRequest('photo')->toMediaCollection('profile');
+        }
+        if (is_array($request->file('covers')) and count($request->file('covers')) > 0){
+            foreach ($request->file('covers') as $image) {
+                $row->addMedia($image)->toMediaCollection('cover');
+            }
+        }
+
+        return redirect('admin/marasi')->with('message', trans('labels.labels.added_successfully'))->with('status', 'success');
+    }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $data['marasi'] =Marasi::find($id);
+        $data['countries'] = Countries::select('id', 'name_' . App::getLocale() . ' as name')->where('status', 1)->get();
+        $data['cities'] = Cities::select('id', 'name_' . App::getLocale() . ' as name')->where('status', 1)->get();
+        return view('admin.marasi.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $rule = [
+            'name_en' => 'required|string',
+            'name_ar' => 'required|string',
+            'description_en' => 'required|string',
+            'description_ar' => 'required|string',
+            'address_en' => 'required|string',
+            'address_ar' => 'required|string',
+            'price' => 'required|numeric',
+            'photo' => 'nullable|image|mimes:png,jpg,jpeg|max:2048',
+            'country_id' => 'required|int|exists:countries,id',
+            'city_id' => 'required|int|exists:cities,id',
+            'longitude' => 'required|string',
+            'latitude' => 'required|string',
+            'covers' => 'nullable|array|min:1',
+            'covers.*' => 'image|mimes:jpeg,png|max:1024', // Validate each image
+        ];
+
+        $validate = Validator::make($request->all(), $rule);
+        if ($validate->fails()) {
+            return redirect()->back()->with('message', $validate->messages()->first())->with('status', 'error')->withInput($request->all());
+        }
+
+        $data = Marasi::find($request->id);
+        $data->update([
+            'name_en'=>$request->name_en,
+            'name_ar'=>$request->name_ar,
+            'description_en'=>$request->description_en,
+            'description_ar'=>$request->description_ar,
+            'address_en'=>$request->address_en,
+            'address_ar'=>$request->address_ar,
+            'longitude'=>$request->longitude,
+            'latitude'=>$request->latitude,
+            'price'=>$request->price,
+            'country_id'=>$request->country_id,
+            'city_id'=>$request->city_id,
+            'is_discount'=>is_null($request->is_discount) ? 0 : 1,
+            'discount_value'=>is_null($request->is_discount) ? 0 : $request->discount,
+            'status' => $request->status ?? 0
+        ]);
+
+        if($request->hasFile('photo') && $request->file('photo')->isValid()){
+            Media::where('model_type',Marasi::class)->where('model_id',$request->id)->where('collection_name','profile')->delete();
+            $data->addMediaFromRequest('photo')->toMediaCollection('profile');
+        }
+
+        if (is_array($request->file('covers')) and count($request->file('covers')) > 0){
+            foreach ($request->file('covers') as $image) {
+                $data->addMedia($image)->toMediaCollection('cover');
+            }
+        }
+        return redirect('admin/marasi')->with('message', trans('labels.labels.modified_successfully'))->with('status', 'success');
+    }
+
+    public function updateStatus(Request $request)
+    {
+
+        try {
+            $marasi = Marasi::find($request->id);
+            $marasi->is_approved = $request->status;
+            $marasi->save();
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'error']);
+        }
+        return response()->json(['message' => 'success']);
+
+    }
+
+    public function destroy(Request $request)
+    {
+        try {
+            Marasi::whereIn('id', $request->id)->delete();
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'error']);
+        }
+        return response()->json(['message' => 'success']);
+    }
+}
---
+++ resources/views/admin/marasi/create.blade.php
@@ -0,0 +1,235 @@
+@extends('admin.layout.master')
+
+@section('css')
+@endsection
+
+@section('style')
+    <style>
+        .hidden {
+            display: none;
+        }
+    </style>
+@endsection
+
+@section('breadcrumb')
+<div class="page-title d-flex flex-column justify-content-center gap-1 me-3 pt-6">
+    <!--begin::Title-->
+    <h1 class="page-heading d-flex flex-column justify-content-center text-dark fw-bold fs-3 m-0">{{trans('labels.labels.add_new')}}</h1>
+    <!--end::Title-->
+    <!--begin::Breadcrumb-->
+    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0">
+        <!--begin::Item-->
+        <li class="breadcrumb-item text-muted">
+            <a href="{{route('admin.marasi.index')}}" class="text-muted text-hover-primary">{{transAdmin('Marasi')}}</a>
+        </li>
+        <!--end::Item-->
+        <!--begin::Item-->
+        <li class="breadcrumb-item">
+            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+        </li>
+        <!--end::Item-->
+        <!--begin::Item-->
+        <li class="breadcrumb-item text-muted">{{trans('labels.labels.add_new')}}</li>
+        <!--end::Item-->
+    </ul>
+    <!--end::Breadcrumb-->
+</div>
+@endsection
+
+@section('content')
+
+    <div id="kt_app_content_container" class="app-container container-fluid">
+        <div class="card mb-5 mb-xl-10">
+            <!--begin::Content-->
+            <div id="kt_account_settings_profile_details" class="collapse show">
+                <!--begin::Form-->
+                <form action="{{route('admin.marasi.store')}}" method="POST" enctype="multipart/form-data" id="kt_account_profile_details_form" class="form">
+                    @csrf
+                    <!--begin::Card body-->
+                    <div class="card-body border-top p-9">
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label fw-semibold fs-6">{{trans('labels.inputs.image')}}</label>
+                            <div class="col-lg-8">
+                                <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url('assets/media/svg/avatars/blank.svg')">
+                                    <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{asset('dash/assets/media/avatars/blank.png')}})"></div>
+                                    <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+                                        <i class="bi bi-pencil-fill fs-7"></i>
+                                        <input type="file" name="photo" accept=".png, .jpg, .jpeg" />
+                                        <input type="hidden" name="avatar_remove" />
+                                    </label>
+                                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel avatar">
+                                        <i class="bi bi-x fs-2"></i>
+                                    </span>
+                                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
+                                        <i class="bi bi-x fs-2"></i>
+                                    </span>
+                                </div>
+
+                                <div class="form-text">Allowed file types: png, jpg, jpeg.</div>
+                            </div>
+                        </div>
+                        @if (Auth::guard('admin')->user()->role_id == 1)
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label fw-semibold fs-6">
+                                    <span class="required">{{transAdmin('Owner')}}</span>
+                                </label>
+                                <div class="col-lg-8 fv-row">
+                                    <select name="employee_id" aria-label="{{trans('labels.inputs.select')}} {{transAdmin('Owner')}}" data-control="select2" data-placeholder="{{trans('labels.inputs.select')}} {{transAdmin('Owner')}}..." class="form-select form-select-solid form-select-lg fw-semibold">
+                                        <option value="">{{trans('labels.inputs.select')}}</option>
+                                        @foreach($data['owner'] as $owner)
+                                            <option value="{{$owner->id}}">{{$owner->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        @else
+                            <input type="hidden" name="employee_id" value="{{Auth::guard('admin')->user()->id}}">
+                        @endif
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.name')}} EN</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="name_en" placeholder="{{trans('labels.inputs.name')}} EN" value="{{old('name_en')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.name')}} AR</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="name_ar" placeholder="{{trans('labels.inputs.name')}} AR" value="{{old('name_ar')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label fw-semibold fs-6">
+                                <span class="required">{{trans('labels.inputs.country')}}</span>
+                            </label>
+                            <div class="col-lg-8 fv-row">
+                                <select name="country_id" aria-label="{{trans('labels.inputs.select')}} {{trans('labels.inputs.country')}}" data-control="select2" data-placeholder="{{trans('labels.inputs.select')}} {{trans('labels.inputs.country')}}..." class="form-select form-select-solid form-select-lg fw-semibold">
+                                    <option value="">{{trans('labels.inputs.select')}}</option>
+                                    @foreach($data['countries'] as $country)
+                                        <option value="{{$country->id}}">{{$country->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label fw-semibold fs-6">
+                                <span class="required">{{trans('labels.inputs.city')}}</span>
+                            </label>
+                            <div class="col-lg-8 fv-row">
+                                <select name="city_id" aria-label="{{trans('labels.inputs.select')}} {{trans('labels.inputs.country')}}" data-control="select2" data-placeholder="{{trans('labels.inputs.select')}} {{trans('labels.inputs.country')}}..." class="form-select form-select-solid form-select-lg fw-semibold">
+                                    <option value="">{{trans('labels.inputs.select')}}</option>
+                                    @foreach($data['cities'] as $city)
+                                        <option value="{{$city->id}}">{{$city->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.description')}} EN</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea name="description_en" placeholder="{{trans('labels.inputs.description')}} EN" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" >{{old('description_en')}}</textarea>
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.description')}} AR</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea name="description_ar" placeholder="{{trans('labels.inputs.description')}} AR" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" >{{old('description_ar')}}</textarea>
+                            </div>
+                        </div>
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.address')}} EN</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea name="address_en" placeholder="{{trans('labels.inputs.address')}} EN" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" >{{old('address_en')}}</textarea>
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.address')}} AR</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea name="address_ar" placeholder="{{trans('labels.inputs.address')}} AR" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" >{{old('address_ar')}}</textarea>
+                            </div>
+                        </div>
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.price')}}</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="number" step=0.01 name="price" placeholder="{{trans('labels.inputs.price')}}" value="{{old('price')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <label class="col-lg-2 col-form-label fw-semibold fs-6"> {{transAdmin('Is Discount ?')}}</label>
+                            <div class="col-lg-8 d-flex align-items-center">
+                                <div class="form-check form-check-solid form-switch form-check-custom fv-row">
+                                    <input class="form-check-input w-45px h-30px" type="checkbox" name="is_discount" id="myCheckbox"  onchange="toggleInput('myCheckbox','myInput')"/>
+                                    <label class="form-check-label" for="myCheckbox"></label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-6 hidden" id="myInput">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.discount')}}</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="discount" placeholder="{{trans('labels.inputs.discount')}}" value="{{old('discount')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.longitude')}}</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="longitude" placeholder="{{trans('labels.inputs.longitude')}}" value="{{old('longitude')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('labels.inputs.latitude')}}</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="latitude" placeholder="{{trans('labels.inputs.latitude')}}" value="{{old('latitude')}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                            </div>
+                        </div>
+
+                        <div class="row mb-6">
+                            <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{transAdmin('Cover Images')}}</label>
+                            <div class="col-lg-8 fv-row">
+                                <input type="file" name="covers[]" placeholder="{{transAdmin('Cover Images')}}" accept=".png, .jpg, .jpeg" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" multiple="multiple" />
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <label class="col-lg-2 col-form-label fw-semibold fs-6"> {{trans('labels.inputs.status')}}</label>
+                            <div class="col-lg-8 d-flex align-items-center">
+                                <div class="form-check form-check-solid form-switch form-check-custom fv-row">
+                                    <input class="form-check-input w-45px h-30px" type="checkbox" name="status" value="1" id="allowmarketing" checked="checked" />
+                                    <label class="form-check-label" for="allowmarketing"></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer d-flex justify-content-end py-6 px-9">
+                        <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">{{trans('labels.inputs.save')}}</button>
+                    </div>
+                    <!--end::Actions-->
+                </form>
+                <!--end::Form-->
+            </div>
+            <!--end::Content-->
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+    <script>
+        function toggleInput($x,$y) {
+            var checkbox = document.getElementById($x);
+            var divElement = document.getElementById($y);
+
+            if (checkbox.checked) {
+                divElement.classList.remove("hidden");
+            } else {
+                divElement.classList.add("hidden");
+            }
+        }
+    </script>
+@endsection
